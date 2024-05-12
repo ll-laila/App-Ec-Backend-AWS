c6ee5930dfeaa1df8ccd17866f80af095b8dc501
ll-laila <lailatimasli1@gmail.com>
2024-05-12 12:16:07 +0100

    Deploy to EC2

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
+
+app.get('/', (req, res) => {
+  res.send('Hello World, from express');
+})
+
+
+/*app.use(cors({
   origin: ['http://localhost:3000',],
   credentials: true
 }));
 
+*/
 app.use("/",express.static("uploads"));
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 
