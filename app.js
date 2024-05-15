0fb79a6563d2b3f63005509e2fafd4e20d5ebdc2
ll-laila <lailatimasli1@gmail.com>
2024-05-15 11:48:52 +0100

    Deploy Node to ec2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://13.60.53.166:3000',],
+  origin: ['http://16.171.3.223:3000',],
   credentials: true
 }));
 
